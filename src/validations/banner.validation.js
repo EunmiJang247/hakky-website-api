2e11f27f8b796f593c4b297a1194bcf6167c2735
고태형 <gotaehyeong@gotaehyeong-ui-MacBookPro.local>
2023-06-23 18:52:43 +0900

    [add] banner route

--- src/validations/banner.validation.js
+++ src/validations/banner.validation.js
@@ -2,22 +2,22 @@ const Joi = require('joi');
 
 const updateBanner = {
   body: Joi.object().keys({
-    banner1: Joi.object.keys({
+    banner1: Joi.object().keys({
       name: Joi.string(),
       image: Joi.string(),
       link: Joi.string(),
     }),
-    banner2: Joi.object.keys({
+    banner2: Joi.object().keys({
       name: Joi.string(),
       image: Joi.string(),
       link: Joi.string(),
     }),
-    banner3: Joi.object.keys({
+    banner3: Joi.object().keys({
       name: Joi.string(),
       image: Joi.string(),
       link: Joi.string(),
     }),
-    banner4: Joi.object.keys({
+    banner4: Joi.object().keys({
       name: Joi.string(),
       image: Joi.string(),
       link: Joi.string(),
