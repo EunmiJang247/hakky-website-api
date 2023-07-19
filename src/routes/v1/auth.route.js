e9e3940beeba52bb5130b4c3575410b643d02199
kht038 <kht038@naver.com>
2023-07-19 18:40:58 +0900

    update auth route

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -14,7 +14,7 @@ router.post('/logout', validate(authValidation.logout), authController.logout);
 router.post('/refresh-token', validate(authValidation.refreshTokens), authController.refreshTokens);
 router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
 router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
-router.post('/subAdmin/reset-password', auth('subAdmin'), validate(authValidation.resetPassword), authController.resetPassword);
+router.post('/subAdmin/reset-password', auth('SUB_ADMIN'), validate(authValidation.resetPassword), authController.resetPassword);
 router.post('/verify-email', validate(authValidation.verifyEmail), authController.verifyEmail);
 router.post('/check-token', auth(), authController.checkToken);
 
