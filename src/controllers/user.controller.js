a51292407284ee3f0596d03d444d4d3591031c5f
kht038 <kht038@naver.com>
2023-08-08 17:56:47 +0900

    update kakao register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -48,7 +48,7 @@ const deleteUser = catchAsync(async (req, res) => {
 });
 
 const deleteUserByAdmin = catchAsync(async (req, res) => {
-  await userService.deleteUserById(req.params.id);
+  await userService.deleteUserById(req.params.userId);
   res.status(httpStatus.NO_CONTENT).end();
 });
 
