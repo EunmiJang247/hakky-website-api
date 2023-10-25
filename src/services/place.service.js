1f32498f35c0d8a00580d714bf0fc1d44df3c62c
고태형 <gotaehyeong@gotaehyeong-ui-MacBookPro.local>
2023-10-25 15:27:14 +0900

    [update] save

--- src/services/place.service.js
+++ src/services/place.service.js
@@ -212,7 +212,6 @@ const getPlaceDetail = async (id, date, dayOfWeek) => {
     placeId: id,
     reservationDate: date,
     isCanceled: false,
-    depositDeadline: { $gt: new Date() },
   }).sort('reservationFrom').exec();
   const includeSchedules = await PlaceIdle.IncludeSchedule.findOne({
     place: id,
