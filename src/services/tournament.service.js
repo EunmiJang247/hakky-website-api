c0714874dc7558e99d7dae02a715a4e4bc446406
softforyou <softforyou247@gmail.com>
2023-12-19 18:10:36 +0900

    calendar-tournament-show

--- src/services/tournament.service.js
+++ src/services/tournament.service.js
@@ -1,5 +1,5 @@
 // const httpStatus = require('http-status');
-const { Tournament } = require('../models');
+const { Tournament, Team } = require('../models');
 // const ApiError = require('../utils/ApiError');
 
 /**
@@ -20,17 +20,40 @@ const queryTournaments = async ({ divisionId, limit, skip }) => {
   };
 };
 
-// const queryTournamentsCalendar = async ({ startDate, endDate }) => {
-//   const tournaments = await Tournament.find({ divisionId }).limit(limit).skip(skip);
-//   const count = await Tournament.countDocuments();
-//   return {
-//     result: tournaments,
-//     count,
-//   };
-// };
+const calendarSerializer = async (tournament) => {
+  const homeTeam = await Team.findById(tournament.homeTeamId);
+  const awayTeam = await Team.findById(tournament.awayTeamId);
+
+  return {
+    id: tournament._id,
+    tournamentDate: tournament.tournamentDate,
+    awayTeamId: tournament.awayTeamId,
+    homeTeamId: tournament.homeTeamId,
+    referee: tournament.referee,
+    supervisor: tournament.supervisor,
+    time: tournament.time,
+    venuePlace: tournament.venuePlace,
+    divisionId: tournament.divisionId,
+    createdAt: tournament.createdAt,
+    updatedAt: tournament.updatedAt,
+    homeTeamName: homeTeam.name,
+    awayTeamName: awayTeam.name,
+  };
+};
+
+const queryTournamentsCalendar = async ({ startDate, endDate }) => {
+  const tournaments = await Tournament.find({
+    tournamentDate: {
+      $gt: startDate,
+      $lt: endDate,
+    },
+  });
+  const result = await Promise.all(tournaments.map(calendarSerializer));
+  return result;
+};
 
 module.exports = {
   createTournament,
   queryTournaments,
-  // queryTournamentsCalendar,
+  queryTournamentsCalendar,
 };
